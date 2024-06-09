42e28def1b80525b3417a0daed372c9688ae9efc
RayenRk <rakkadrayen@gmail.com>
2024-06-09 23:54:08 +0100

    📜Intro section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} relative bg-gray-50 text-gray-950`}>
+      <body className={`${inter.className} relative bg-gray-50 text-gray-950 pt-28 h-[5000px] sm:pt-36`}>
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 
+        <Header />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import Intro from "@/components/Intro";
+
 export default function Home() {
   return (
-    <main className="">
-     
+    <main className="flex flex-col items-center px-4">
+     <Intro />
     </main>
   );
 }

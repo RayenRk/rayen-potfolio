274b8161474a6a09c2eae935b6839d76102b9ef2
RayenRk <rakkadrayen@gmail.com>
2024-08-11 19:04:56 +0100

    🟨 resumé update

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -92,7 +92,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Rayen_Rakkad_Software_Engineer_Resume_NoPhoto.pdf"
+          href="/Rayen_Rakkad_Software_Engineer_Resume.pdf"
           download
         >
           Download my CV{" "}
